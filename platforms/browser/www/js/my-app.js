f37c110e691959e9be7c7f037229efad13039b68
Aleksey <u608110@gmai.com>
2017-06-06 18:19:33 +0300

    Deleting Task Workout

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -1,9 +1,9 @@
 // Initialize app
 var myApp = new Framework7();
-
+var db;
 
 // If we need to use custom DOM library, let's save it to $$ variable:
-var $$ = Dom7;
+let $$ = Dom7;
 
 // Add view
 var mainView = myApp.addView('.view-main', {
@@ -11,16 +11,107 @@ var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
 });
 
+
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
+    db = window.openDatabase('workouttracker', '1.0', 'Workout Tracker', 1000000);
+    createDatabase();
+    getWorkouts();
 });
 
 
+myApp.onPageInit('add', function (page) {
+    $$('#workout-form').on('submit', function(e){
+        var data = {
+            id:guidGenerator(),
+            title: $$('#title').val(),
+            date: $$('#date').val(),
+            type: $$('#type').val(),
+            length: $$('#length').val(),
+        };
 
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
-
+        addWorkout(data);
+    });
 });
 
+function createDatabase(){
+    db.transaction(createTable,
+        function(tx, err){
+            alert('DB Error: '+err);
+        },
+        function(){
+            console.log('Database & Table Created...');
+        });
+}
+
+function createTable(tx){
+    //tx.executeSql('DROP TABLE IF EXISTS workouts');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS workouts (id unique, title, date, type, length)');
+}
+
+function guidGenerator() {
+    var S4 = function() {
+        return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
+
+function addWorkout(workout){
+    db.transaction(function(tx){
+            tx.executeSql('INSERT INTO workouts (id, title, date, type, length) VALUES("'+workout.id+'","'+workout.title+'","'+workout.date+'","'+workout.type+'","'+workout.length+'")');
+        },
+        function(err){
+            console.log(err);
+        },
+        function(){
+            window.location.href='index.html';
+        });
+}
+
+function getWorkouts(){
+    db.transaction(function(tx){
+        tx.executeSql('SELECT * FROM workouts ORDER BY date DESC', [],
+            function(tx, results){
+                var len = results.rows.length;
+                console.log('workouts table: '+len+' rows found');
+                for(let i = 0; i < len; i++){
+                    $$('#workout-list').append(`
+          <li class="swipeout remove-callback" id="${results.rows.item(i).id}">
+            <a href="details.html?id=${results.rows.item(i).id}" class="item-link swipeout-content item-content">
+              <div class="item-inner">
+                <div class="item-title">${results.rows.item(i).title}</div>
+                <div class="item-after">${results.rows.item(i).date}</div>
+              </div>
+            </a>
+            <div class="swipeout-actions-right">
+              <a href="#" class="swipeout-delete">Delete</a>
+            </div>
+          </li>
+        `);
+                }
+            },
+            function(err){
+                console.log(err);
+            });
+    });
+
+    // Delete Event
+    $$(document.on('deleted', '.remove-callback', function () {
+        var workoutId = $$(this).attr('id');
+
+        deleteWorkout(workoutId);
+    }));
+
+    function deleteWorkout () {
+        db.transaction(function (tx) {
+            tx.executeSql('DELETE FROM workouts WHERE id="'+id+'"');
+        }, 
+        function (err) {
+            console.log(err);
+        },
+        function () {
+            console.log('Workout Deleted');
+        });
+    }
+}
